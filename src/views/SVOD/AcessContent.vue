fa1965ab85747662f5fb3c8b211fb2520706aa89
truest422 <truest422@gmail.com>
2023-04-18 02:18:36 +0900

    detail page mobile

--- src/views/SVOD/AcessContent.vue
+++ src/views/SVOD/AcessContent.vue
@@ -1,69 +1,140 @@
 <template>
-    <div class="d-flex" v-if="this.getPackage && this.getPackage.length > 0" style="margin-top:150px;margin-left: 100px;" @click="navigationStateChange()">
-        <v-img :src="isDev?CardVod:this.packageObj[0].package_image" width="400" style="border-radius: 10px;"></v-img>
-        <div class="d-flex flex-column justify-start text-left align-start px-16" style="width: 65vw;">
-            <div class="d-flex justify-start align-center mb-5">
-                <v-img :src="this.packageObj[0].content_provider_logo" width="50" height="50" style="border-radius: 50%;" class="mr-3"></v-img>
-                <div class="d-flex justify-start align-start text-subtitle-1">
-                    <!-- {{ item.title }} -->
-                    {{ this.packageObj[0].package_name }}
+    <div v-if="phoneWidth <= 560">
+        <div class="d-flex" v-if="this.getPackage && this.getPackage.length > 0"
+            style="margin: 10px; flex-direction:column" @click="navigationStateChange()">
+            <v-img :src="isDev ? CardVod : this.packageObj[0].package_image" width="400" style="border-radius: 10px;"></v-img>
+            <div class="d-flex flex-column justify-start text-center align-start px-1" style="width: 90vw; margin-top:10px">               
+
+                <div class="text-subtitle-1 justify-start text-start align-center">
+                    <p class="text-caption text--disabled" style="width: 90vw height:100%">
+                        <!-- {{ item.subtitle }} -->
+                        {{ this.packageObj[0].package_description }}
+                    </p>
+                </div>
+
+                <div class="d-flex justify-start align-center mb-5 mt-3">
+                    <v-img :src="this.packageObj[0].content_provider_logo" width="50" height="50"
+                        style="border-radius: 50%;" class="mr-3"></v-img>
+                    <div class="d-flex justify-start align-start text-subtitle-1">
+                        <!-- {{ item.title }} -->
+                        {{ this.packageObj[0].package_name }}
+                    </div>
                 </div>
             </div>
+            <div class="d-flex flex-row justify-start" style="align-items: center; padding:10px">
+                
+                <div class="text-h5 mr-10">
+                    <!-- {{ item.title }} -->
+                    ${{ this.packageObj[0].package_price }}
+                    <!-- <p class="text-caption text--disabled">{{ item.subtitle }}</p> -->
+                </div>
+                <div class="text-subtitle-1 round-md">
+                    <!-- {{ item.price }} -->
 
-            <div class="text-subtitle-1 justify-center align-start">
-                <p class="text-caption text--disabled">
-                    <!-- {{ item.subtitle }} -->
-                    {{this.packageObj[0].package_description}}
-                </p>
+                    <v-btn depressed color="white" :style="ButtonColor"
+                        @click="gotoSubscriptionUrl(this.packageObj[0].package_subscription_url)">
+                        Access Option
+                    </v-btn>
+                </div>
             </div>
         </div>
-        <div class="d-flex flex-column justify-start pa-10 mr-10" style="align-items: center;">
-            <div class="text-subtitle-1 mb-1">
-                <!-- {{ item.title }} -->
-                Subscription
-                <!-- <p class="text-caption text--disabled">{{ item.subtitle }}</p> -->
-            </div>
-            <div class="text-h5 mb-6">
-                <!-- {{ item.title }} -->
-                ${{this.packageObj[0].package_price}}
-                <!-- <p class="text-caption text--disabled">{{ item.subtitle }}</p> -->
-            </div>
-            <div class="text-subtitle-1 round-md">
-                <!-- {{ item.price }} -->
-
-                <v-btn depressed color="white" :style="ButtonColor" @click="gotoSubscriptionUrl(this.packageObj[0].package_subscription_url)">
-                    Access Option
-                </v-btn>
-            </div>
+        <div>
+            <v-divider class="mb-5"></v-divider>
+            <v-row v-if="moviesInPackage && moviesInPackage.length > 0">
+                <v-col cols="12" md="12">
+                    <h4 class="text-left ml-10 fcw">
+                        <!-- {{category.categoryName}} -->
+                        Films
+                    </h4>
+                </v-col>
+                <v-col cols="12" md="12">
+                    <SVODAccessContents :categoryList="moviesInPackage" type="movies" />
+                </v-col>
+            </v-row>
+            <v-row v-if="seriesInPackage && seriesInPackage.length > 0">
+                <v-col cols="12" md="12">
+                    <h4 class="text-left ml-10 fcw">
+                        <!-- {{category.categoryName}} -->
+                        Series
+                    </h4>
+                </v-col>
+                <v-col cols="12" md="12">
+                    <SVODAccessContents :categoryList="seriesInPackage" type="series" />
+                </v-col>
+            </v-row>
         </div>
     </div>
-    <div>
-        <div class="text-h6 mb-2">
-            All content included in Subscription
+
+    <div v-else>
+        <div class="d-flex" v-if="this.getPackage && this.getPackage.length > 0"
+            style="margin-top:150px;margin-left: 100px;" @click="navigationStateChange()">
+            <v-img :src="isDev ? CardVod : this.packageObj[0].package_image" width="400" style="border-radius: 10px;"></v-img>
+            <div class="d-flex flex-column justify-start text-left align-start px-16" style="width: 65vw;">
+                <div class="d-flex justify-start align-center mb-5">
+                    <v-img :src="this.packageObj[0].content_provider_logo" width="50" height="50"
+                        style="border-radius: 50%;" class="mr-3"></v-img>
+                    <div class="d-flex justify-start align-start text-subtitle-1">
+                        <!-- {{ item.title }} -->
+                        {{ this.packageObj[0].package_name }}
+                    </div>
+                </div>
+
+                <div class="text-subtitle-1 justify-center align-start">
+                    <p class="text-caption text--disabled">
+                        <!-- {{ item.subtitle }} -->
+                        {{ this.packageObj[0].package_description }}
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex flex-column justify-start pa-10 mr-10" style="align-items: center;">
+                <div class="text-subtitle-1 mb-1">
+                    <!-- {{ item.title }} -->
+                    Subscription
+                    <!-- <p class="text-caption text--disabled">{{ item.subtitle }}</p> -->
+                </div>
+                <div class="text-h5 mb-6">
+                    <!-- {{ item.title }} -->
+                    ${{ this.packageObj[0].package_price }}
+                    <!-- <p class="text-caption text--disabled">{{ item.subtitle }}</p> -->
+                </div>
+                <div class="text-subtitle-1 round-md">
+                    <!-- {{ item.price }} -->
+
+                    <v-btn depressed color="white" :style="ButtonColor"
+                        @click="gotoSubscriptionUrl(this.packageObj[0].package_subscription_url)">
+                        Access Option
+                    </v-btn>
+                </div>
+            </div>
         </div>
-        <v-divider class="mb-5"></v-divider>
-        <v-row v-if="moviesInPackage && moviesInPackage.length > 0">
-            <v-col cols="12" md="12">
-                <h4 class="text-left ml-10 fcw">
-                    <!-- {{category.categoryName}} -->
-                    Films
-                </h4>
-            </v-col>
-            <v-col cols="12" md="12">
-                <SVODAccessContents :categoryList="moviesInPackage" type="movies" />
-            </v-col>
-        </v-row>
-        <v-row v-if="seriesInPackage && seriesInPackage.length > 0">
-            <v-col cols="12" md="12">
-                <h4 class="text-left ml-10 fcw">
-                    <!-- {{category.categoryName}} -->
-                    Series
-                </h4>
-            </v-col>
-            <v-col cols="12" md="12">
-                <SVODAccessContents :categoryList="seriesInPackage" type="series" />
-            </v-col>
-        </v-row>
+        <div>
+            <div class="text-h6 mb-2">
+                All content included in Subscription
+            </div>
+            <v-divider class="mb-5"></v-divider>
+            <v-row v-if="moviesInPackage && moviesInPackage.length > 0">
+                <v-col cols="12" md="12">
+                    <h4 class="text-left ml-10 fcw">
+                        <!-- {{category.categoryName}} -->
+                        Films
+                    </h4>
+                </v-col>
+                <v-col cols="12" md="12">
+                    <SVODAccessContents :categoryList="moviesInPackage" type="movies" />
+                </v-col>
+            </v-row>
+            <v-row v-if="seriesInPackage && seriesInPackage.length > 0">
+                <v-col cols="12" md="12">
+                    <h4 class="text-left ml-10 fcw">
+                        <!-- {{category.categoryName}} -->
+                        Series
+                    </h4>
+                </v-col>
+                <v-col cols="12" md="12">
+                    <SVODAccessContents :categoryList="seriesInPackage" type="series" />
+                </v-col>
+            </v-row>
+        </div>
     </div>
 </template> 
   
@@ -94,6 +165,7 @@ export default {
     },
     data() {
         return {
+            phoneWidth:window.innerWidth,
             PlayIcon: PlayIcon,
             CardVod: CardVod,
             iconLogo: iconLogo,
@@ -174,7 +246,7 @@ export default {
     computed: {
         getPackage() {
             this.packageObj = this.$store.state.PackageStore.packages;
-            if(this.packageObj.length > 0) {
+            if (this.packageObj.length > 0) {
                 // var moviesString = this.packageObj[0].MoviesInPackage;
                 // this.moviesInPackage = moviesString.split(",");
                 // var seriesString = this.packageObj[0].SeriesInPackage;
